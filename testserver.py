183c4033d46f8e6c194283d4e793cc5d25ba1672
Monika Johanna <hannas1027@hotmail.com>
2021-10-06 16:07:30 -0500

    Update testserver.py

--- testserver.py
+++ testserver.py
@@ -1,10 +1,10 @@
 from flask import Flask
 	
-	app = Flask(__name__)
+app = Flask(__name__)
 	
-	@app.route('/')
-	def home():
-	    return "Comprobando que funciona el deploy"
+@app.route('/')
+def home():
+	return "Comprobando que funciona el deploy"
 	
-	if __name__ == "__main__":
-	    app.run()
+if __name__ == "__main__":
+	app.run()
